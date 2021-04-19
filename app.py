78d3c59510374db7d8d8b80049713721f58d0659
codezart <abdur.rahman.00798@gmail.com>
2021-04-19 19:29:36 +0300

    testing

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def csvPreProcessing():
             with open("Earn.json", "w") as outfile: 
                 json.dump(earn_dict, outfile)
     
-            return jsonify({"income":earn_dict})
+            return json.dumps({"income":earn_dict})
 
         else: 
             return "file not found"
