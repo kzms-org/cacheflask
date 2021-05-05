72170ac63c4d45c65a866ce5cfb3b34df0455e27
codezart <abdur.rahman.00798@gmail.com>
2021-05-05 19:46:38 +0300

    cv

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Original file is located at
 # imports
 
 from prophet.serialize import model_to_json, model_from_json
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import json
 import pandas as pd
 import numpy as np
